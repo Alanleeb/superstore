be4b5553248054999a18e5b70936015e10d7c4ca
Alan Bischoff <alanlbischoff@gmail.com>
2018-05-30 17:07:30 -0600

    store with departments, styled

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'departments/index'
-  get 'departments/show'
-  get 'departments/new'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+root 'department#index'
+
+resources :departments do
+  resources :items
+end
 end
